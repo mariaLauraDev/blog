4062e90f98b451287e40e9508cde0da2675cd83a
@laura.alves <laura.alves@gocase.com.br>
2024-02-10 19:34:45 -0300

    feat: CRUD blog_posts table

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,54 @@
+class BlogPostsController < ApplicationController
+  before_action :set_blog_post, except: [:index, :new, :create] #only: %i[show edit update destroy]
+
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def show
+  rescue ActiveRecord::RecordNotFound
+    # render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+    redirect_to root_path
+  end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  end
+end
